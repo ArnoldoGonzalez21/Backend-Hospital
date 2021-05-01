8fdde442480ded3cf3a2031e4d8fcdf415d4afe8
Your Name <you@example.com>
2021-05-01 15:47:25 -0600

    Estado de la cita

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ def responder_cita():
     global citas
     global pacientes
     j = int(indice)
-    if j == 2 or j == 3:
+    if estado == '2' or estado == '3':
         pacientes[j].estado = False
     citas[i].cambiar_estado_cita(indice, estado)
     return jsonify({'agregado':7,'mensaje':'Estado de la Cita Cambiado Exitosamente'})
